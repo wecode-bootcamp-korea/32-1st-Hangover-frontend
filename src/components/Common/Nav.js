9293f10541d23ae5849ebc818b4a13d2a79234cd
u-rim <99340550+u-rim@users.noreply.github.com>
2022-04-26 18:41:34 +0900

    nav 사이즈 조정

--- src/components/Common/Nav.js
+++ src/components/Common/Nav.js
@@ -1,7 +1,43 @@
+import React from 'react';
 import './Nav.scss';
 
 const Nav = () => {
-  return <header>this is nav</header>;
+  return (
+    <header>
+      <div className="navOutLine">
+        <div className="upperNav">
+          <div className="layout">
+            <img src="/images/logo.png" alt="Logopng" className="logo" />
+            {/* <i class="fa-solid fa-magnifying-glass" /> */}
+            <input
+              className="inputNav"
+              type="text"
+              placeholder="🔍   Search any alcohol"
+            />
+          </div>
+
+          <div className="upperRightNav">
+            <span>ship to</span>
+            <i class="fa-solid fa-angle-down" />
+            <span>Langauage</span>
+            <i class="fa-solid fa-angle-down" />
+            <i class="fa-regular fa-user" />
+          </div>
+        </div>
+        <div className="downNav">
+          <i class="fa-solid fa-wine-glass" />
+          <span>주류</span>
+          <i class="fa-solid fa-burger" />
+          <span>소개</span>
+          <i class="fa-solid fa-check" />
+          <span>영역1</span>
+          <i class="fa-solid fa-check" />
+          <span>영역2</span>
+        </div>
+        <hr className="bottomNavLine" />
+      </div>
+    </header>
+  );
 };
 
 export default Nav;
--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -1,7 +1,28 @@
+import React from 'react';
 import './ProductList.scss';
 
 const ProductList = () => {
-  return <div>produvt</div>;
+  return (
+    <>
+      <div className="list">
+        <h1>리스트 페이지 카피영역</h1>
+        <span>From 1 regional wine style</span>
+        <button className="sort" type="button">
+          리뷰
+        </button>
+        <i class="fa-solid fa-chevron-down" />
+      </div>
+      <article>
+        <div className="cardLayout">
+          <div className="filter"></div>
+          <div className="bigCard">
+            <img src="/images/won.png" />
+            <span className="cardLayout">WonSoju</span>
+          </div>
+        </div>
+      </article>
+    </>
+  );
 };
 
 export default ProductList;
