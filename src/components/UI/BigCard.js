bec2eca071d6d3cc5494d18eed5a7aa6cc4d6277
BEOOM <wnsqja39@gmail.com>
2022-04-29 18:47:04 +0900

    big card 작업완료

--- src/components/UI/BigCard.js
+++ src/components/UI/BigCard.js
@@ -1,7 +1,40 @@
 import './BigCard.scss';
 
 const BigCard = () => {
-  <div>this is BigCard</div>;
+  return (
+    <div className="bigCardsContainer">
+      <div className="container">
+        <div className="bigCardId">
+          <div className="bottleContainer">
+            <div className="bottleImgContainer">
+              <img src="images/dummy.png" className="cardBottleImg" />
+            </div>
+          </div>
+
+          <div className="wineInfoContainer">
+            <div className="wineInfo">
+              <span>Donnafugata</span>
+              <span className="originCounter">Tancredi 2016</span>
+            </div>
+            <div className="drinkOrigin">
+              <div>
+                <img />
+                <span>Terre Siciliane, Italy</span>
+              </div>
+            </div>
+          </div>
+          <div className="drinkRatingContainer">
+            <div className="drinkRating">
+              <div>4.1</div>
+              <div>별점</div>
+              <div>1410 ratings</div>
+            </div>
+            <div>Available online from </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BigCard;
