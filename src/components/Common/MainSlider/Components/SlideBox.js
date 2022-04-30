20e9ea278a1caea0edc826e8094f041134d5624a
leeheejuuun <94230809+leeheejuuun@users.noreply.github.com>
2022-05-01 00:36:49 +0900

    메인페이지 캐러셀 레이아웃작업

--- src/components/Common/MainSlider/Components/SlideBox.js
+++ src/components/Common/MainSlider/Components/SlideBox.js
@@ -7,10 +7,15 @@ export default function SlideBox() {
   const carousel = useRef();
 
   const prevBtnClick = () => {
-    setSlideid(slideid >= 1 ? slideid - 1 : slideid); // -1000, 0
+    setSlideid(slideid >= 1 || slideid === 3 ? slideid - 1 : slideid); // -1000, 0
+    // carousel.current.style.transform = `translate(${-1000 * slideid}px)`; // 1, 0
+
+    // console.log(`slideid:${slideid}`);
+    // console.log(`slideid2:${slideid2}`);
   };
   const nextBtnClick = () => {
     setSlideid(slideid < 2 ? slideid + 1 : slideid); // -1000, -2000
+    // carousel.current.style.transform = `translate(${-1000 * slideid}px)`;
   };
 
   useEffect(() => {
@@ -18,65 +23,73 @@ export default function SlideBox() {
   }, [slideid]);
 
   return (
-    <div className="slideWrap">
-      <h2>title</h2>
-
-      <div className="carousel">
-        <ul ref={carousel}>
-          <li>
-            <img src="../images/dummy.png"></img>
-            <div className="text">1</div>
-          </li>
-          <li>
-            <img src="../images/dummy.png"></img>
-            <div className="text">2</div>
-          </li>
-          <li>
-            <img src="../images/dummy.png"></img>
-            <div className="text">3</div>
-          </li>
-          <li>
-            <img src="../images/dummy.png"></img>
-            <div className="text">4</div>
-          </li>
-          <li>
-            <img src="../images/dummy.png"></img>
-            <div className="text">5</div>
-          </li>
-          <li>
-            <img src="../images/dummy.png"></img>
-            <div className="text">6</div>
-          </li>
-          <li>
-            <img src="../images/dummy.png"></img>
-            <div className="text">7</div>
-          </li>
-          <li>
-            <img src="../images/dummy.png"></img>
-            <div className="text">7</div>
-          </li>
-          <li>
-            <img src="../images/dummy.png"></img>
-            <div className="text">7</div>
-          </li>
-          <li>
-            <img src="../images/dummy.png"></img>
-            <div className="text">7</div>
-          </li>
-          <li>
-            <img src="../images/dummy.png"></img>
-            <div className="text">7</div>
-          </li>
-          <li>
-            <img src="../images/dummy.png"></img>
-            <div className="text">12</div>
-          </li>
-        </ul>
-      </div>
-      <button slideid={slideid} onClick={prevBtnClick}>
+    <div className="wrap">
+      <button className="slideLeftBtn" slideid={slideid} onClick={prevBtnClick}>
         1
       </button>
-      <button slideid={slideid} onClick={nextBtnClick}>
+
+      <div className="slideWrap">
+        <h2>title</h2>
+
+        <div className="carousel">
+          <ul ref={carousel}>
+            <li>
+              <img src="../images/dummy.png" />
+              <div className="text">1</div>
+            </li>
+            <li>
+              <img src="../images/dummy.png" />
+              <div className="text">2</div>
+            </li>
+            <li>
+              <img src="../images/dummy.png" />
+              <div className="text">3</div>
+            </li>
+            <li>
+              <img src="../images/dummy.png" />
+              <div className="text">4</div>
+            </li>
+            <li>
+              <img src="../images/dummy.png" />
+              <div className="text">5</div>
+            </li>
+            <li>
+              <img src="../images/dummy.png" />
+              <div className="text">6</div>
+            </li>
+            <li>
+              <img src="../images/dummy.png" />
+              <div className="text">7</div>
+            </li>
+            <li>
+              <img src="../images/dummy.png" />
+              <div className="text">8</div>
+            </li>
+            <li>
+              <img src="../images/dummy.png" />
+              <div className="text">9</div>
+            </li>
+            <li>
+              <img src="../images/dummy.png" />
+              <div className="text">10</div>
+            </li>
+            <li>
+              <img src="../images/dummy.png" />
+              <div className="text">11</div>
+            </li>
+            <li>
+              <img src="../images/dummy.png" />
+              <div className="text">12</div>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <button
+        className="slideRigthBtn"
+        slideid={slideid}
+        onClick={nextBtnClick}
+      >
         2
       </button>
     </div>
--- src/components/Common/MainSlider/Components/SlideCard.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import '../Components/SlideCard.scss';
-
-export default function SlideCard() {
-  return <></>;
-}
