adf5a2df037c75fcab8e0928dd576537caa61aca
leeheejuuun <94230809+leeheejuuun@users.noreply.github.com>
2022-05-01 17:06:33 +0900

    메인페이지 캐러셀 구조화 완료

--- src/components/Common/MainSlider/Components/SlideBox.js
+++ src/components/Common/MainSlider/Components/SlideBox.js
@@ -1,97 +1,74 @@
 import React, { useEffect, useRef, useState } from 'react';
 import '../Components/SlideBox.scss';
-// import SlideCard from './SlideCard';
+
 export default function SlideBox() {
+  const [cardLists, setCardList] = useState([]);
   const [slideid, setSlideid] = useState(0);
-  // const [slideid2, setSlideid2] = useState(0);
   const carousel = useRef();
 
   const prevBtnClick = () => {
-    setSlideid(slideid >= 1 || slideid === 3 ? slideid - 1 : slideid); // -1000, 0
-    // carousel.current.style.transform = `translate(${-1000 * slideid}px)`; // 1, 0
-
-    // console.log(`slideid:${slideid}`);
-    // console.log(`slideid2:${slideid2}`);
+    setSlideid(slideid >= 1 || slideid === 3 ? slideid - 1 : slideid);
   };
   const nextBtnClick = () => {
-    setSlideid(slideid < 2 ? slideid + 1 : slideid); // -1000, -2000
-    // carousel.current.style.transform = `translate(${-1000 * slideid}px)`;
+    setSlideid(slideid < 2 ? slideid + 1 : slideid);
   };
 
   useEffect(() => {
     carousel.current.style.transform = `translate(${-1000 * slideid}px)`;
   }, [slideid]);
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        return setCardList(data);
+      });
+  }, []);
   return (
-    <div className="wrap">
-      <button className="slideLeftBtn" slideid={slideid} onClick={prevBtnClick}>
-        1
-      </button>
-
-      <div className="slideWrap">
-        <h2>title</h2>
-
-        <div className="carousel">
-          <ul ref={carousel}>
-            <li>
-              <img src="../images/dummy.png" />
-              <div className="text">1</div>
-            </li>
-            <li>
-              <img src="../images/dummy.png" />
-              <div className="text">2</div>
-            </li>
-            <li>
-              <img src="../images/dummy.png" />
-              <div className="text">3</div>
-            </li>
-            <li>
-              <img src="../images/dummy.png" />
-              <div className="text">4</div>
-            </li>
-            <li>
-              <img src="../images/dummy.png" />
-              <div className="text">5</div>
-            </li>
-            <li>
-              <img src="../images/dummy.png" />
-              <div className="text">6</div>
-            </li>
-            <li>
-              <img src="../images/dummy.png" />
-              <div className="text">7</div>
-            </li>
-            <li>
-              <img src="../images/dummy.png" />
-              <div className="text">8</div>
-            </li>
-            <li>
-              <img src="../images/dummy.png" />
-              <div className="text">9</div>
-            </li>
-            <li>
-              <img src="../images/dummy.png" />
-              <div className="text">10</div>
-            </li>
-            <li>
-              <img src="../images/dummy.png" />
-              <div className="text">11</div>
-            </li>
-            <li>
-              <img src="../images/dummy.png" />
-              <div className="text">12</div>
-            </li>
-          </ul>
+    <>
+      <div className="wrap">
+        <button
+          className="slideLeftBtn"
+          slideid={slideid}
+          onClick={prevBtnClick}
+        >
+          1
+        </button>
+        <div className="slideWrap">
+          <span className="hangOverTopLank">HangOver TopLank 12</span>
+          <div className="carousel">
+            <ul ref={carousel}>
+              {cardLists.map(cardList => {
+                return (
+                  <li>
+                    <img
+                      src="../../../../../public/images/dummy.png"
+                      alt="cardImg"
+                    />
+                    <div className="cardTextWrap">
+                      <h2>취하노 지수</h2>
+                      <div className="cardBottomText">
+                        <p>술 종류</p>
+                        <p>술 이름</p>
+                        <p>원산지</p>
+                      </div>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
+        <button
+          className="slideRigthBtn"
+          slideid={slideid}
+          onClick={nextBtnClick}
+        >
+          2
+        </button>
       </div>
-
-      <button
-        className="slideRigthBtn"
-        slideid={slideid}
-        onClick={nextBtnClick}
-      >
-        2
-      </button>
-    </div>
+    </>
   );
 }
