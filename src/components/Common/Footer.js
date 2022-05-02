b4dc277d3f406450d3a052089f88d3eb7aceaab1
BEOOM <wnsqja39@gmail.com>
2022-05-02 13:57:24 +0900

    코멘트 반영수정

--- src/components/Common/Footer.js
+++ src/components/Common/Footer.js
@@ -1,113 +1,156 @@
 import './Footer.scss';
 
+const FOOTER_BULLET_DATA = [
+  {
+    id: 1,
+    name: 'footerBulletStyle',
+    icon: 'fa-solid fa-check',
+    text: "Shop the world's largest wine marketplace",
+    span: 'footerBulletInfo',
+  },
+  {
+    id: 2,
+    name: 'footerBulletStyle',
+    icon: 'fa-brands fa-whatsapp',
+    text: 'Our support team is always here to help',
+    span: 'footerBulletInfo',
+  },
+  {
+    id: 3,
+    name: 'footerBulletStyle',
+    icon: 'fa-solid fa-truck',
+    text: 'Careful delivery right to your doorstep',
+    span: 'footerBulletInfo',
+  },
+  {
+    id: 4,
+    name: 'footerBulletStyle',
+    icon: 'fa-regular fa-handshake',
+    text: 'Check honest reviews of any wine before purchase',
+    span: 'footerBulletInfo',
+  },
+];
+
+const FOOTER_MAP_DATA = [
+  {
+    id: 1,
+    name: 'APP',
+  },
+  {
+    id: 2,
+    name: 'About',
+  },
+  {
+    id: 3,
+    name: 'Contact',
+  },
+  {
+    id: 4,
+    name: 'Drink News',
+  },
+  {
+    id: 5,
+    name: 'Drink Style Awards',
+  },
+  {
+    id: 6,
+    name: 'Merchants',
+  },
+  {
+    id: 7,
+    name: 'Sponsorships',
+  },
+  {
+    id: 8,
+    name: 'Become an Affiliate',
+  },
+  {
+    id: 9,
+    name: 'careers',
+  },
+  {
+    id: 10,
+    name: 'Terms of Use',
+  },
+  {
+    id: 11,
+    name: 'Content of Policy',
+  },
+  {
+    id: 12,
+    name: 'Sitemap',
+  },
+];
 const Footer = () => {
-  const footerBullet = [
-    {
-      id: 1,
-      name: 'footerBulletOne',
-      icon: 'fa-solid fa-check',
-      text: "Shop the world's largest wine marketplace",
-      span: 'footerBulletInfo',
-    },
-    {
-      id: 2,
-      name: 'footerBulletTwo',
-      icon: 'fa-brands fa-whatsapp',
-      text: 'Our support team is always here to help',
-      span: 'footerBulletInfo',
-    },
-    {
-      id: 3,
-      name: 'footerBulletThree',
-      icon: 'fa-solid fa-truck',
-      text: 'Careful delivery right to your doorstep',
-      span: 'footerBulletInfo',
-    },
-    {
-      id: 4,
-      name: 'footerBulletFour',
-      icon: 'fa-regular fa-handshake',
-      text: 'Check honest reviews of any wine before purchase',
-      span: 'footerBulletInfo',
-    },
-  ];
   return (
-    <footer>
-      <div className="footerContainer">
-        <div className="footerTopContainer">
-          <div className="footerTop">
-            <div className="a">
-              <h1 className="footerHeader">
-                Trusted by millions to discover and buy the right drink every
-                time.
-              </h1>
-              <div className="footerBullet">
-                {footerBullet.map(({ id, name, icon, text, span }) => {
-                  return (
-                    <div className={name} key={id}>
-                      <div>
-                        <i className={icon}></i>
-                      </div>
-                      <span className={span}>{text}</span>
+    <footer className="footerContainer">
+      <div className="footerTopContainer">
+        <div className="footerTop">
+          <div className="footerTopMaxWidth">
+            <h1 className="footerHeader">
+              Trusted by millions to discover and buy the right drink every
+              time.
+            </h1>
+            <div className="footerBullet">
+              {FOOTER_BULLET_DATA.map(({ id, name, icon, text, span }) => {
+                return (
+                  <div className={name} key={id}>
+                    <div>
+                      <i className={icon} />
                     </div>
-                  );
-                })}
-              </div>
+                    <span className={span}>{text}</span>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
-        <section className="b">
-          <div className="footerBottom">
-            <div className="downloadContainer">
-              <div className="downloadAppBox">
-                <div className="downloadApp">
-                  <i className="fa-solid fa-apple-whole"></i>
-                </div>
-                <div className="downloadInfo">
-                  <span>Download on the</span>
-                  <span className="appleStore">Apple Store</span>
-                </div>
+      </div>
+      <section className="footerBottom">
+        <div className="footerBottomMaxWidth">
+          <div className="downloadContainer">
+            <div className="downloadAppBox">
+              <div className="downloadApp">
+                <i className="fa-solid fa-apple-whole" />
               </div>
-              <div className="downloadAppBox">
-                <div className="downloadApp">
-                  <i className="fa-brands fa-android"></i>
-                </div>
-                <div className="downloadInfo">
-                  <span>Download on the</span>
-                  <span className="appleStore">Galaxy Store</span>
-                </div>
+              <div className="downloadInfo">
+                <span>Download on the</span>
+                <span className="appleStore">Apple Store</span>
               </div>
-
-              <div className="footerSocialCopyright">
-                <div className="footerSns">
-                  <i className="fa-brands fa-instagram"></i>
-                  <i className="fa-brands fa-facebook"></i>
-                  <i className="fa-brands fa-twitter"></i>
-                </div>
-                <span className="footerCopyright">@Chuihano 2022</span>
+            </div>
+            <div className="downloadAppBox">
+              <div className="downloadApp">
+                <i className="fa-brands fa-android" />
+              </div>
+              <div className="downloadInfo">
+                <span>Download on the</span>
+                <span className="appleStore">Galaxy Store</span>
               </div>
             </div>
 
-            <div className="footerMapSocial">
-              <div className="footerMap">
-                <a link="#">App</a>
-                <a link="#">About</a>
-                <a link="#">Contact</a>
-                <a link="#">Press</a>
-                <a link="#">Wine News</a>
-                <a link="#">Wine Style Awards</a>
-                <a link="#">Merchants</a>
-                <a link="#">Sponsorships</a>
-                <a link="#">Become an Affiliate</a>
-                <a link="#">careers</a>
-                <a link="#">Terms of Use</a>
-                <a link="#">Content Policy</a>
-                <a link="#">Sitemap</a>
+            <div className="footerSocialCopyright">
+              <div className="footerSns">
+                <i className="fa-brands fa-instagram" />
+                <i className="fa-brands fa-facebook" />
+                <i className="fa-brands fa-twitter" />
               </div>
+              <span className="footerCopyright">@Chuihano 2022</span>
             </div>
           </div>
-        </section>
-      </div>
+
+          <div className="footerMapSocial">
+            <div className="footerMap">
+              {FOOTER_MAP_DATA.map(({ id, name }) => {
+                return (
+                  <a link="#" key={id}>
+                    {name}
+                  </a>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </section>
     </footer>
   );
 };
