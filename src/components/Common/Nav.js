eb8d496c6678b72ca12438024bee384a3e9086a6
leeheejuuun <94230809+leeheejuuun@users.noreply.github.com>
2022-05-02 17:14:57 +0900

    검색기능 구현 작업중

--- src/components/Common/Nav.js
+++ src/components/Common/Nav.js
@@ -1,42 +1,91 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Nav.scss';
-
+// import LoginJoin from './LoginAndJoin/LoginJoin/LoginJoin';
 const Nav = () => {
+  const [login, setLogin] = useState(false);
+  const [searchInputList, setSearchInputList] = useState([]);
+  const [userInput, setUserInput] = useState('');
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        return setSearchInputList(data);
+      });
+  }, []);
+
+  const handleChange = e => {
+    return setUserInput(e.target.value);
+  };
+
+  const search = userInput.length
+    ? searchInputList.filter(list => {
+        return list.name.toLowerCase().includes(userInput.toLowerCase());
+      })
+    : [];
+
   return (
-    <header>
-      <div className="navOutLine">
-        <div className="upperNav">
-          <div className="layout">
-            <img src="/images/logo.png" alt="Logopng" className="logo" />
-            {/* <i class="fa-solid fa-magnifying-glass" /> */}
+    <nav className="nav">
+      <div className="upperNav">
+        <div className="layout">
+          <img src="/images/logo.png" alt="Logopng" className="logo" />
+          <div className="searchInput">
             <input
               className="inputNav"
               type="text"
               placeholder="🔍   Search any alcohol"
+              onChange={handleChange}
             />
-          </div>
-
-          <div className="upperRightNav">
-            <span>ship to</span>
-            <i class="fa-solid fa-angle-down" />
-            <span>Langauage</span>
-            <i class="fa-solid fa-angle-down" />
-            <i class="fa-regular fa-user" />
+            <ul className="inputList">
+              {search.map(list => {
+                return (
+                  <li key={list.id}>
+                    <img
+                      src={`https://robohash.org/${list.id}?set=set2&size=180x180`}
+                    />
+                    <span>{list.name}</span>
+                    <span>{list.name}</span>
+                  </li>
+                );
+              })}
+            </ul>
+            <div>
+              <style jsx="true">{`
+                body {
+                  overflow: hidden;
+                }
+              `}</style>
+            </div>
           </div>
         </div>
-        <div className="downNav">
-          <i class="fa-solid fa-wine-glass" />
-          <span>주류</span>
-          <i class="fa-solid fa-burger" />
-          <span>소개</span>
-          <i class="fa-solid fa-check" />
-          <span>영역1</span>
-          <i class="fa-solid fa-check" />
-          <span>영역2</span>
+        <div className="upperRightNav">
+          <span>ship to</span>
+          <i className="fa-solid fa-angle-down" />
+          <span>Langauage</span>
+          <i className="fa-solid fa-angle-down" />
+          <i
+            className="fa-regular fa-user"
+            onClick={() => {
+              login === false ? setLogin(true) : setLogin(false);
+            }}
+          />
         </div>
-        <hr className="bottomNavLine" />
       </div>
-    </header>
+      <div className="downNav">
+        <i className="fa-solid fa-wine-glass" />
+        <span>주류</span>
+        <i className="fa-solid fa-burger" />
+        <span>소개</span>
+        <i className="fa-solid fa-check" />
+        <span>영역1</span>
+        <i className="fa-solid fa-check" />
+        <span>영역2</span>
+      </div>
+      <hr className="bottomNavLine" />
+
+      {/* {login && <LoginJoin setLogin={setLogin} login={login} />} */}
+    </nav>
   );
 };
 
