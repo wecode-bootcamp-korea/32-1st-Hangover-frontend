f0b1c9a7ad41826dff5219b685ee53369704bcac
leeheejuuun <94230809+leeheejuuun@users.noreply.github.com>
2022-05-04 13:38:15 +0900

    Nav 수정중

--- src/components/Common/Nav.js
+++ src/components/Common/Nav.js
@@ -1,28 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DrinkMenu from './DrinkMenu';
+import LoginJoin from '../Common/LoginAndJoin/LoginJoin/LoginJoin';
 import './Nav.scss';
 
 const Nav = () => {
   const [visible, setVisible] = useState(false);
+  const [login, setLogin] = useState(false);
+  const [searchInputList, setSearchInputList] = useState([]);
+  const [userInput, setUserInput] = useState('');
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        return setSearchInputList(data);
+      });
+  }, []);
+
+  const handleChange = e => {
+    return setUserInput(e.target.value);
+  };
+
+  const search = userInput.length
+    ? searchInputList.filter(list => {
+        return list.name.toLowerCase().includes(userInput.toLowerCase());
+      })
+    : [];
+
   return (
     <nav className="nav">
       <div className="upperNav">
         <div className="layout">
-          <a href="/">
-            <img src="/images/logo.png" alt="Logopng" className="logo" />
-          </a>
-          <input
-            className="inputNav"
-            type="text"
-            placeholder="🔍   Search any alcohol"
-          />
+          <img src="/images/logo.png" alt="Logopng" className="logo" />
+          <div className="searchInput">
+            <input
+              className="inputNav"
+              type="text"
+              placeholder="🔍   Search any alcohol"
+              onChange={handleChange}
+            />
+            <ul className="inputList">
+              {search.map(list => {
+                return (
+                  <li key={list.id}>
+                    <img
+                      src={`https://robohash.org/${list.id}?set=set2&size=180x180`}
+                      art="img"
+                    />
+                    <div>
+                      <p className="alcoholName">{list.name}</p>
+                      <p className="alcoholCategoris">{list.name}</p>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
+
         <div className="upperRightNav">
           <span>ship to</span>
           <i class="fa-solid fa-angle-down" />
           <span>Langauage</span>
           <i class="fa-solid fa-angle-down" />
-          <i class="fa-regular fa-user" />
+          <i
+            className="fa-regular fa-user"
+            onClick={() => {
+              login === false ? setLogin(true) : setLogin(false);
+            }}
+          />
         </div>
       </div>
       <div className="downNav">
@@ -50,6 +97,7 @@ const Nav = () => {
         </div>
       </div>
       <hr className="bottomNavLine" />
+      {login && <LoginJoin setLogin={setLogin} login={login} />}
     </nav>
   );
 };
