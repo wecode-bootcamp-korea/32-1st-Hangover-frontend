c5b4bb7de16eee5e5fd746fb7b612bf88e332206
leeheejuuun <94230809+leeheejuuun@users.noreply.github.com>
2022-05-05 18:22:07 +0900

    캐러셀 버튼 수정완료

--- src/components/Common/MainCarousel/MainCarousel.js
+++ src/components/Common/MainCarousel/MainCarousel.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import Carousel from '../MainCarousel/Components/CarouselList';
+import CarouselList from '../MainCarousel/Components/CarouselList';
 import './MainCarousel.scss';
-export default function SlideBox() {
+export default function MainCarousel() {
   const [hangOverLists, sethangOverLists] = useState([]);
   const [ramdomList, setRamdomList] = useState([]);
 
@@ -31,13 +31,13 @@ export default function SlideBox() {
         <source src="/images/Beer - 80133 (online-video-cutter.com).mp4" />
       </video>
       <span className="hangOverTopRank">HangOver TopRank 12</span>
-      <Carousel hangOverLists={hangOverLists} />
+      <CarouselList hangOverLists={hangOverLists} />
 
       <video autoPlay muted loop>
         <source src="/images/Wine - 56914.mp4" />
       </video>
       <span className="recommendList">Recommendation of the week 12</span>
-      <Carousel hangOverLists={ramdomList} />
+      <CarouselList hangOverLists={ramdomList} />
     </div>
   );
 }
